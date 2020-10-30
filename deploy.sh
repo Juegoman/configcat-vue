0e3a7184cadf8c0824533cb91048d31f74219426
Ruben Gallegos <ruben.gallegos@freestar.io>
2020-10-30 16:26:19 -0700

    cut out a bunch of stuff, trying to fix build issues

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#Run this script to update configcat-common to the latest and release a new version of configcat-js
-set -e #Making sure script stops on error
-npm i configcat-common@latest
-npm test
-git add .
-git commit -m "updating configcat-common"
-git push origin $(npm version minor)
-git push
